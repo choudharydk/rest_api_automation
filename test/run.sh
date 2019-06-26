6cf00bae0b7cc4bcf613b50505fba3f116dbc414
Dhruv Choudhary <dhruv.1si12cs038@gmail.com>
2019-06-26 18:58:13 +0530

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -2,7 +2,7 @@
 echo "************************"
 echo "Testing API REST..."
 echo "************************"
-NOW=$(date  +%d-%m-%y-%H:%M) && ./node_modules/mocha/bin/mocha  test/get*.js --timeout 100000 --reporter mochawesome --reporter-options reportFilename=test-${NOW} #--reporter mocha-simple-html-reporter --reporter-options output=report.html
+NOW=$(date  +%d-%m-%y-%H:%M) && ./node_modules/mocha/bin/mocha  test/**/*.js --timeout 100000 --reporter mochawesome --reporter-options enableCode=false, reportFilename=test-${NOW}
 echo "************************"
 echo "Killing background processes..."
 echo "************************"
