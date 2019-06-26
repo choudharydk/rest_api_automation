ed10c3c54e11dad5e03a04a1ecf7e5c569843042
Dhruv Choudhary <dhruv.1si12cs038@gmail.com>
2019-06-26 18:29:34 +0530

    Update getService.js

--- test/getService.js
+++ test/getService.js
@@ -1,11 +1,19 @@
 const chai = require('chai');
 const expect = chai.expect;
-const request = require("../utils/httpServer");
-const config = require("../config");
-const header = require('../utils/header');
+const should = chai.should;
+const request = require("../../utils/httpServer");
+const config = require("../../config");
+const header = require('../../utils/header');
+
+/* to log the info, require below modules */
+var log4js = require("log4js");
+var log = require("../../utils/logger")
+log4js.configure(log.logging());
+var logger = log4js.getLogger();
+
 
 describe("GET Service API ", () => {
-
+    logger.info("GET SErvice API TEST Starts")
     let baseUrl = config.baseUrl;
     let url;
     let headers = header.plainHeader();
@@ -19,8 +27,29 @@ describe("GET Service API ", () => {
             .requestPromiseQuery(url, 'GET', headers)
             .then((response) => {
                 //console.log(response.body)
+                logger.info("Number of Records in GET Service API with url ", url, "is ", response.body.length)
                 expect(response.statusCode).to.equal(200);
                 expect(response.body.length).to.be.at.least(100)
+            })
+            .then(() => done(), done)
+            .catch((error) => {
+                done(error);
+            });
+    })
+
+    it(" verify the response schema", (done) => {
+        let uri = "/posts";
+
+        url = baseUrl + uri;
+        console.log("url is", url)
+        logger.info(" verify the response schema ")
+        request
+            .requestPromiseQuery(url, 'GET', headers)
+            .then((response) => {
+                logger.info("status code in GET Service API with url ", url, "is ", JSON.stringify(response.statusCode))
+                expect(response.statusCode).to.equal(200);
+                expect(response.body).to.be.an.instanceof(Object);
+                response.body.every(i => expect(i).to.have.all.keys('body', 'id', 'title', 'userId'))
             })
             .then(() => done(), done)
             .catch((error) => {
@@ -38,10 +67,9 @@ describe("GET Service API ", () => {
             .then((response) => {
                 //to store the size of response, as it is object ,  convert it to array 
                 let responseArray = [response.body]
-                
-                console.log("response size is", responseArray, responseArray.length)
+                logger.info("response in GET Service API when querying 1 record  with url ", url, "is ", JSON.stringify(responseArray))
                 expect(response.statusCode).to.equal(200);
-                expect(responseArray).to.equal(1);
+                expect(responseArray.length).to.equal(1);
                 expect(response.body.id).to.equal(1)
             })
             .then(() => done(), done)
@@ -54,18 +82,27 @@ describe("GET Service API ", () => {
         let uri = "/posts/invalidPost";
 
         url = baseUrl + uri;
+
         console.log("url is", url)
         request
-            .requestPromiseQuery(url, 'GET', headers)
-            .then((response) => {
-                expect(response.statusCode).to.equal(404);
-                expect(responseArray).to.equal(1);
-                expect(response.body.id).to.equal(1)
+            .requestQuery(url, "GET", headers, function (err, resp) {
+                if (err) {
+                    //console.log("err for empty data is ", err)
+                    logger.error("Error in GET Service API with invalid url ", url, "is ", JSON.stringify(resp.body))
+                    done();
+
+                } else if (!(/^2/.test('' + resp.statusCode))) { // Status Codes other than 2xx
+                    //  console.log("status code error for empty data is ",resp, resp.statusCode)
+                    logger.info("response in GET Service API with invalid url ", url, "is ", JSON.stringify(resp.body))
+                    logger.info("status code  in GET Service API with invalid url ", url, "is ", JSON.stringify(resp.statusCode))
+
+                    expect(resp.statusCode).to.equal(404)
+                    done();
+
+                }
             })
-            .then(() => done(), done)
-            .catch((error) => {
-                done(error);
-            });
     })
 
+    logger.info("GET SErvice API TEST ENDS")
+    logger.info("*****************************************")
 })
